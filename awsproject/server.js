c55f0cbc8153d7898fe0527aac35e2d4e9bd4f7d
Conor Thorne <thornec@Conors-MacBook-Air.local>
2020-12-06 17:27:50 +0000

    cleaned code and fixed few issues

--- awsproject/server.js
+++ awsproject/server.js
@@ -2,37 +2,32 @@
 var express = require('express');
 var path = require('path');  
 var bodyParser = require("body-parser");          
-const { response } = require('express');
 var AWS = require("aws-sdk");
-var fs = require('fs');
-const app = express();
-const port = 3000;
+/* Constants */
+const app = express();                      // framework for API 
+const port = 3000;                          // port number
+const bucket = "csu44000assign2useast20"    // bucket with movie json
+const key = "moviedata.json"                // key for bucket
+const S3 = new AWS.S3();                    // create aws object
 
-const bucket = "csu44000assign2useast20"
-const key = "moviedata.json"
-const S3 = new AWS.S3();
-
-
-
+// transcript of table creation
 exports.handler = async (event, context, callback) => {
     var transcript = await download();
     console.log(transcript);
 }
-
 
 /* run express server on port 3000 */
 app.listen(port, () => {
     console.log(`Server running at http://localhost:3000/`);
   });
 
-
+// connect to dyanamo server
 AWS.config.update({
     region: "us-east-1",
 });
 
-
+// display vue index
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
-
 
 // create movie table & loading data into it
 app.post('/newtable', (req, res) => {
@@ -42,40 +37,33 @@ app.post('/newtable', (req, res) => {
 
         // set parameters
         var params = {
-
-        TableName : "Movies",
-        KeySchema: [       
-            { AttributeName: "year", KeyType: "HASH"},    // Partition key
-            { AttributeName: "title", KeyType: "RANGE" }  // Sort key
-        ],
-        AttributeDefinitions: [       
-            { AttributeName: "year", AttributeType: "N" },
-            { AttributeName: "title", AttributeType: "S" }
-        ],
-        ProvisionedThroughput: {       
-            ReadCapacityUnits: 5, 
-            WriteCapacityUnits: 5
-        }
+            TableName : "Movies",
+            KeySchema: [       
+                { AttributeName: "year", KeyType: "HASH"},    // Partition key
+                { AttributeName: "title", KeyType: "RANGE" }  // Sort key
+            ],
+            AttributeDefinitions: [       
+                { AttributeName: "year", AttributeType: "N" },
+                { AttributeName: "title", AttributeType: "S" }
+            ],
+            ProvisionedThroughput: {       
+                ReadCapacityUnits: 5, 
+                WriteCapacityUnits: 5
+            }
     };
 
     // create table with parameters
     dynamodb.createTable(params, function(err, data) {
-        // check for errors
-        if (err) {
-            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-        }
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
     });
     
-    // load data into created db
+    // call function to load movie json data into table
     load_data();
 })
 
-// load data into database
+// load movie json data into database
 function load_data(){
-    var docClient = new AWS.DynamoDB.DocumentClient();
-
+    var docClient = new AWS.DynamoDB.DocumentClient();              // create client
     console.log("Importing movies into DynamoDB. Please wait.");
 
     // set key and bucket details
@@ -87,57 +75,43 @@ function load_data(){
 
     // retrieve JSON from bucket
     S3.getObject(data_options, (error,data) => {
-            if(error){
-                console.log("error" + error);
-            }
-            else{
-                //response.send(data);
-                const moviedata = JSON.parse((data.Body).toString());
-                console.log(moviedata);
 
-                // add movies to table
-                var allMovies = moviedata;
-                allMovies.forEach(function(movie) {
-                    var params = {
-                        TableName: "Movies",
-                        Item: {
-                            "year":  movie.year,
-                            "title": movie.title,
-                            "release_date":  movie.info.release_date,
-                            "rank": movie.info.rank
-                        }
-                    };
+            const moviedata = JSON.parse((data.Body).toString());       // parse json
+            console.log(moviedata);
 
-                    docClient.put(params, function(err, moviedata) {
-                    if (err) {
-                        console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
-                    } else {
-                        console.log("PutItem succeeded:", movie.title);
+            var allMovies = moviedata;
+            // for each movie in json add it to the talbe
+            allMovies.forEach(function(movie) {
+                // add movie title, year, release date and rank to Movies table
+                var params = {
+                    TableName: "Movies",
+                    Item: {
+                        "year":  movie.year,
+                        "title": movie.title,
+                        "release_date":  movie.info.release_date,
+                        "rank": movie.info.rank
                     }
-                
+                };
+                // put movie data into client
+                docClient.put(params, function(err, moviedata) {
+                    console.log("PutItem succeeded:", movie.title);
                 });
-            });
-
-            }
-        
+         });
     });
-        
-    //body: JSON.stringify(data);
-    //console.log(body);
 }
 
-//app.post('/query', queryTable);
+// extensions to read json and spaces
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
+// endpoint for querying on front end
 app.post('/query',(req, res) => {
 
-    var docClient = new AWS.DynamoDB.DocumentClient();
-    console.log(req.body)
-    let name = req.body.name;
-    let year = parseInt(req.body.year);
+    var docClient = new AWS.DynamoDB.DocumentClient();      // create client
+    let name = req.body.name;               // movie name
+    let year = parseInt(req.body.year);     // movie year
     
-
+    // params for querying table
     var params = {
         TableName : "Movies",
         ProjectionExpression:"#yr, title, #rk, release_date",
@@ -151,7 +125,7 @@ app.post('/query',(req, res) => {
             ":movie_title": name
         }
     };
-
+    // query data in table
     docClient.query(params, function(err, data) {
     if (err) {
         console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
@@ -160,16 +134,15 @@ app.post('/query',(req, res) => {
         data.Items.forEach(function(item) {
             console.log(" -", item.year + ": " + item.title + "rank:" + item.rank + "release date:" + item.release_date);
         });
-
-        res.json({
-            data : data.Items
-        })
-        
-    }
-    
+            // return data from dyanamo query
+            res.json({
+                data : data.Items
+            })      
+        }    
     });
 })
 
+// delete table endpoint
 app.post('/table', (req, res) => {
 
     var dynamodb = new AWS.DynamoDB();
@@ -187,8 +160,7 @@ app.post('/table', (req, res) => {
         }
     });
 
-    res.json();
-
+    res.json();     // return   
 })
 
 
